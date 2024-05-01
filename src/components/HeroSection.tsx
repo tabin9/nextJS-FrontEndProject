93c0dafa75e1c9e6ce9a852b6740343eab792bc3
Tabin Tariq <tabintariq9@gmail.com>
2024-05-01 18:25:20 +0530

    tiny change in Explore Courses button.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,7 +17,7 @@ function HeroSection() {
                 <Link href={"/courses"}>
                     <Button 
                     borderRadius="1.75rem"
-                    className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800">
+                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800">
                         Explore Courses
                     </Button>
                 </Link>
